846eabf36486d91a393d030da4ff446c0331818c
KolluriVenkataSravani <sravanikv2003@gmail.com>
2024-08-11 12:17:50 +0530

    Div contents replaced by root.render

--- App.js
+++ App.js
@@ -4,17 +4,28 @@
             <h1>"I am h1 tag"</h1>
             <h2>"i am h2 tag"</h2>
         </div>
+        <div id="child2">
+            <h1>"I am h1 tag"</h1>
+            <h2>"i am h2 tag"</h2>
+        </div>
     </div>
 */
 const parent=React.createElement(
     "div",
-    {id:"parent"},
-    React.createElement(
-        "div",
-        {id:"child"},[
-            React.createElement("h1",{},"I am h1 tag"),
-            React.createElement("h2",{},"I am h2 tag")
-        ]));
+    {id:"parent"},[
+        React.createElement(
+            "div",
+            {id:"child"},[
+                React.createElement("h1",{},"I am h1 tag"),
+                React.createElement("h2",{},"I am h2 tag")
+            ]),
+        React.createElement(
+            "div",
+            {id:"child2"},[
+                React.createElement("h1",{},"I am h1 tag"),
+                React.createElement("h2",{},"I am h2 tag")
+            ])
+    ]);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
 // const heading=React.createElement("h1",{id: "heading", abc:"xyz"},"Hello World from React!");// this api takes 3 elements (tag,object-attributes to the tag,content inside h1 tag)

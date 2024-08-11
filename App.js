6262c38410e3198e3adceeb186d655df807a67a8
KolluriVenkataSravani <sravanikv2003@gmail.com>
2024-08-11 11:58:54 +0530

    Multiple children in a div tag

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 /*
     <div id="parent">
         <div id="child">
-            <h1></h1>
+            <h1>"I am h1 tag"</h1>
+            <h2>"i am h2 tag"</h2>
         </div>
     </div>
 */
-const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"I am h1 tag")));
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},[
+            React.createElement("h1",{},"I am h1 tag"),
+            React.createElement("h2",{},"I am h2 tag")
+        ]));
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
 // const heading=React.createElement("h1",{id: "heading", abc:"xyz"},"Hello World from React!");// this api takes 3 elements (tag,object-attributes to the tag,content inside h1 tag)

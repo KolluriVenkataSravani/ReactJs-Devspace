96f5294a96f5f55eb1a11067a3dfbe67af66465f
KolluriVenkataSravani <sravanikv2003@gmail.com>
2024-09-02 22:25:44 +0530

    Add JSX

--- App.js
+++ App.js
@@ -12,27 +12,32 @@ import ReactDOM from "react-dom/client";
         </div>
     </div>
 */
-const parent=React.createElement(
-    "div",
-    {id:"parent"},[
-        React.createElement(
-            "div",
-            {id:"child"},[
-                React.createElement("h1",{},"Sravanii"),
-                React.createElement("h2",{},"I am h2 tag")
-            ]),
-        React.createElement(
-            "div",
-            {id:"child2"},[
-                React.createElement("h1",{},"I am h1 tag"),
-                React.createElement("h2",{},"I am h2 tag")
-            ])
-    ]);
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// const parent=React.createElement(
+//     "div",
+//     {id:"parent"},[
+//         React.createElement(
+//             "div",
+//             {id:"child"},[
+//                 React.createElement("h1",{},"Sravanii"),
+//                 React.createElement("h2",{},"I am h2 tag")
+//             ]),
+//         React.createElement(
+//             "div",
+//             {id:"child2"},[
+//                 React.createElement("h1",{},"I am h1 tag"),
+//                 React.createElement("h2",{},"I am h2 tag")
+//             ])
+//     ]);
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
 // const heading=React.createElement("h1",{id: "heading", abc:"xyz"},"Hello World from React!");// this api takes 3 elements (tag,object-attributes to the tag,content inside h1 tag)
 // console.log(heading); //js object
 // const root= ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 //creating an element is the job of react
 //when we are rendering something into root it is the job of ReactDOM
+
+const jsxHeading = <h1 id="heading">Namaste React Using JSX</h1>
+console.log(jsxHeading);
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);

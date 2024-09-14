0de4649821c3e0d2dd3625a6c482b9fbbd1a657e
KolluriVenkataSravani <sravanikv2003@gmail.com>
2024-09-14 19:34:26 +0530

    Add React Components

--- App.js
+++ App.js
@@ -39,5 +39,29 @@ import ReactDOM from "react-dom/client";
 
 const jsxHeading = <h1 id="heading">Namaste React Using JSX</h1>
 console.log(jsxHeading);
+const Title= () =>(
+    <h1 className="head" tabIndex="5">
+        Namaste React Using JSX
+    </h1>
+); // Arrow Function
+/*
+const Title= function(){
+    return(
+        <h1 className="head" tabIndex="5">
+            Namaste React Using JSX
+        </h1>
+    );
+}; // Normal Function
+*/
+const number=10000;
+const HeadingComponent= () =>(
+    <div id="container">
+    <Title />
+    {number}
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+); //Rendering one func component in another func component
 const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+//root.render(jsxHeading);
+root.render(<HeadingComponent />);  //Rendering functional component onto the web page, babel understands this syntax
+

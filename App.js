e59084df2016400dc8c8f72a167253e3090c4dc4
KolluriVenkataSravani <sravanikv2003@gmail.com>
2024-09-28 11:39:25 +0530

    Add Header Component

--- App.js
+++ App.js
@@ -11,31 +11,31 @@ import ReactDOM from "react-dom/client";
             <h2>"i am h2 tag"</h2>
         </div>
     </div>
-*/
-// const parent=React.createElement(
-//     "div",
-//     {id:"parent"},[
-//         React.createElement(
-//             "div",
-//             {id:"child"},[
-//                 React.createElement("h1",{},"Sravanii"),
-//                 React.createElement("h2",{},"I am h2 tag")
-//             ]),
-//         React.createElement(
-//             "div",
-//             {id:"child2"},[
-//                 React.createElement("h1",{},"I am h1 tag"),
-//                 React.createElement("h2",{},"I am h2 tag")
-//             ])
-//     ]);
-// const root=ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
-// const heading=React.createElement("h1",{id: "heading", abc:"xyz"},"Hello World from React!");// this api takes 3 elements (tag,object-attributes to the tag,content inside h1 tag)
-// console.log(heading); //js object
-// const root= ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-//creating an element is the job of react
-//when we are rendering something into root it is the job of ReactDOM
+
+const parent=React.createElement(
+    "div",
+    {id:"parent"},[
+        React.createElement(
+            "div",
+            {id:"child"},[
+                React.createElement("h1",{},"Sravanii"),
+                React.createElement("h2",{},"I am h2 tag")
+            ]),
+        React.createElement(
+            "div",
+            {id:"child2"},[
+                React.createElement("h1",{},"I am h1 tag"),
+                React.createElement("h2",{},"I am h2 tag")
+            ])
+    ]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+const heading=React.createElement("h1",{id: "heading", abc:"xyz"},"Hello World from React!");// this api takes 3 elements (tag,object-attributes to the tag,content inside h1 tag)
+console.log(heading); //js object
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+creating an element is the job of react
+when we are rendering something into root it is the job of ReactDOM
 
 const jsxHeading = <h1 id="heading">Namaste React Using JSX</h1>
 console.log(jsxHeading);
@@ -44,7 +44,7 @@ const Title= () =>(
         Namaste React Using JSX
     </h1>
 ); // Arrow Function
-/*
+
 const Title= function(){
     return(
         <h1 className="head" tabIndex="5">
@@ -52,7 +52,7 @@ const Title= function(){
         </h1>
     );
 }; // Normal Function
-*/
+
 const number=10000;
 const HeadingComponent= () =>(
     <div id="container">
@@ -66,7 +66,38 @@ const HeadingComponent= () =>(
         <h1 className="heading">Namaste React Functional Component</h1>
     </div>
 ); //Rendering one func component in another func component
+*/
+
+const Header= () =>{
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://t3.ftcdn.net/jpg/04/03/74/22/360_F_403742248_8DDzcFF4jw05lWqftk2yxzKRpFvpZ01Y.jpg"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About US</li>
+                    <li>Contact US</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout=()=>{
+    return(
+        <div className="app">
+            <Header />
+        </div>
+    )
+}
+
+
+
+
 const root= ReactDOM.createRoot(document.getElementById("root"));
 //root.render(jsxHeading);
-root.render(<HeadingComponent />);  //Rendering functional component onto the web page, babel understands this syntax
+root.render(<AppLayout />);  //Rendering functional component onto the web page, babel understands this syntax
 

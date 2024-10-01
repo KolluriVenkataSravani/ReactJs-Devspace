73483ded2f037bb40d7e5b83df525b8f3b44224c
KolluriVenkataSravani <sravanikv2003@gmail.com>
2024-10-01 23:50:41 +0530

    Add Live Data Of One Restaurant

--- App.js
+++ App.js
@@ -91,17 +91,129 @@ const StyleCard= {
 }
 
 const RestaurantCard= (props) =>{
-    const {resName, cuisine} = props;
+    const {resData} = props;
     return(
         <div className="res-card" style={StyleCard}>
-            <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/j88ngtan5pxib04das3y"></img>
-            <h3>{props.resName}</h3>
-            <h4>{props.cuisine}</h4>
-            <h4>4.1 stars</h4>
-            <h4>45-50 Mins</h4>
+            <img className="res-logo" alt="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+resData.info.cloudinaryImageId}></img>
+            <h3>{resData.info.name}</h3>
+            <h4>{resData.info.cuisines.join(", ")}</h4>
+            <h4>{resData.info.avgRating} stars</h4>
+            <h4>{resData.info.sla.deliveryTime} minutes</h4>
         </div>
     )
 }
+
+const resObj={
+    "info": {
+      "id": "708880",
+      "name": "KFC",
+      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/17/8ba01310-e5b7-4a90-bef3-98a10bc91238_708880.JPG",
+      "locality": "Sydenhams Road",
+      "areaName": "Periyampet",
+      "costForTwo": "₹400 for two",
+      "cuisines": [
+        "Burgers",
+        "Fast Food",
+        "Rolls & Wraps"
+      ],
+      "avgRating": 4.2,
+      "parentId": "547",
+      "avgRatingString": "4.2",
+      "totalRatingsString": "2.4K+",
+      "sla": {
+        "deliveryTime": 35,
+        "serviceability": "SERVICEABLE",
+        "slaString": "25-35 mins",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-10-01 23:00:00",
+        "opened": true
+      },
+      "badges": {
+        "imageBadges": [
+          {
+            "imageId": "Rxawards/_CATEGORY-Burger.png",
+            "description": "Delivery!"
+          }
+        ]
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            "badgeObject": [
+              {
+                "attributes": {
+                  "description": "Delivery!",
+                  "imageId": "Rxawards/_CATEGORY-Burger.png"
+                }
+              }
+            ]
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "30% OFF",
+        "subHeader": "UPTO ₹75"
+      },
+      "orderabilityCommunication": {
+        "title": {
+          
+        },
+        "subTitle": {
+          
+        },
+        "message": {
+          
+        },
+        "customIcon": {
+          
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      "context": "seo-data-1d5e74a1-d7f4-4095-a97c-45b3fb82dd46"
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/chennai/kfc-sydenhams-road-periyampet-rest708880",
+      "text": "RESTAURANT_MENU",
+      "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  };
 
 const Body= () =>{
     return(
@@ -110,8 +222,7 @@ const Body= () =>{
                 Search
             </div>
             <div className="restro-container">
-                <RestaurantCard resName="Ghuma Ghumalu" cuisine="Biryani, Andhra, Asian" />
-                <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
+                <RestaurantCard resData={resObj} />
             </div>
         </div>
     )

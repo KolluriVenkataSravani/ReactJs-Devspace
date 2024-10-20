f1c4b1da6082c267941f858d12198920d131e55d
KolluriVenkataSravani <sravanikv2003@gmail.com>
2024-10-20 17:59:06 +0530

    Add API data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+// import resList from "../utils/mockData";
+import { useState,useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body= () =>{
-    const [listOfRestaurants,setResList]=useState(resList);
+    const [listOfRestaurants,setResList]=useState([]);
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData= async () => {
+        const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0843007&lng=80.2704622&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json=await data.json();
+        const cardData=json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        setResList(cardData);
+    }
+
+    if(listOfRestaurants.length==0){
+        return <Shimmer />
+    }
+
     return(
         <div className="body">
             <div className="filter">

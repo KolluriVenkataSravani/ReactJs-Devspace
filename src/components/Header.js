57f6b1d86e8b88468ad5b13138de1e39a83b59b3
KolluriVenkataSravani <sravanikv2003@gmail.com>
2024-10-20 18:43:16 +0530

    Add Login,Logout Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header= () =>{
+    const [btnNameReact, setBtnNameReact] = useState("Login");
     return(
         <div className="header">
             <div className="logo-container">
@@ -12,6 +14,9 @@ const Header= () =>{
                     <li>About US</li>
                     <li>Contact US</li>
                     <li>Cart</li>
+                    <button className="login" onClick={()=>{
+                        btnNameReact=="Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login");
+                    }}>{btnNameReact}</button>
                 </ul>
             </div>
         </div>

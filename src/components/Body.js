6cdbc85ef8c69e2f94e52310eeb6029044dbc3af
KolluriVenkataSravani <sravanikv2003@gmail.com>
2024-10-20 18:42:29 +0530

    Add Shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -12,15 +12,16 @@ const Body= () =>{
     const fetchData= async () => {
         const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0843007&lng=80.2704622&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json=await data.json();
-        const cardData=json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        const cardData=json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants; //Optional Chaining
         setResList(cardData);
     }
 
-    if(listOfRestaurants.length==0){
-        return <Shimmer />
-    }
+    // Conditional Rendering
+    // if(listOfRestaurants.length==0){
+    //     return <Shimmer />;
+    // }
 
-    return(
+    return (listOfRestaurants.length==0) ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
                 <button 

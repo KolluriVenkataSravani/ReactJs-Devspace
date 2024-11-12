495362165c332c3436f41579d7746dc6f35de666
KolluriVenkataSravani <sravanikv2003@gmail.com>
2024-11-12 20:27:13 +0530

    Lazy Loading

---
+++ src/components/Grocery.js
@@ -0,0 +1,11 @@
+
+const Grocery = () => {
+  return (
+    <h1>
+        {" "}
+        Our Grocery Online Store, and we have a lot of child components insidethis web page!!!
+    </h1>
+  );
+}
+
+export default Grocery;
